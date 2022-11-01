8bbcb6d39bd84a26362a28690d1084737ff8be51
Victor TOPORAN <victor.toporan@gmail.com>
2022-11-01 09:57:26 +0200

    Adds basic input reading

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -31,6 +31,13 @@ class Home extends StatelessWidget {
                 },
                 child: const Text("L'historie"),
               ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Login()));
+                },
+                child: const Text("L'ogin"),
+              ),
             ],
           ),
         ));
@@ -56,6 +63,63 @@ class History extends StatelessWidget {
               const Text(
                 "History Screen",
                 style: TextStyle(color: Colors.white),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Home"),
+              ),
+            ],
+          ),
+        ));
+  }
+}
+
+class Login extends StatelessWidget {
+  Login({super.key});
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black26,
+        appBar: AppBar(
+          title: const Text("GainsTrack"),
+          centerTitle: true,
+          backgroundColor: Colors.orange[400],
+          foregroundColor: Colors.red[900],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text(
+                "Login Screen",
+                style: TextStyle(color: Colors.white),
+              ),
+              TextField(
+                controller: usernameController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  labelText: "User name",
+                  hintText: "username or email",
+                ),
+              ),
+              TextField(
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                  hintText: "password",
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print(usernameController.text);
+                  print(passwordController.text);
+                },
+                child: const Text("Login"),
               ),
               ElevatedButton(
                 onPressed: () {
