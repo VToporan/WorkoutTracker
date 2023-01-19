4de701f79abce802db15f334919a7c0a6dc0ab63
Victor TOPORAN <victor.toporan@gmail.com>
2023-01-19 23:04:27 +0200

    Adds demo for springboot endpoints

---
+++ backend/springboot/src/main/java/tevi/msa/User.java
@@ -0,0 +1,34 @@
+package tevi.msa;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ backend/springboot/src/main/java/tevi/msa/UserService.java
@@ -0,0 +1,28 @@
+package tevi.msa;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+@Service
+@Transactional
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    public List<User> listAllUser() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
