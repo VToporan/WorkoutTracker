a996e2d211913485b2bdcf81dd7f946e88afd207
Victor TOPORAN <victor.toporan@gmail.com>
2023-01-20 02:47:13 +0200

    Fixes login returning posted data

--- backend/springboot/src/main/java/tevi/msa/UserController.java
+++ backend/springboot/src/main/java/tevi/msa/UserController.java
@@ -33,8 +33,8 @@ public class UserController {
             if (! existingUser.getPassword().equals((Object) user.getPassword())) {
                 throw new DataIntegrityViolationException("Password doesn't match");
             }
-
-            return new ResponseEntity<User>(user, HttpStatus.OK); 
+            return new ResponseEntity<User>(existingUser, HttpStatus.OK); 
+            
         } catch (DataIntegrityViolationException e) {
             return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
         }
