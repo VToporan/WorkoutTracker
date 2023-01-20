a9053244311421c5c19c47c7ff993683fbde95c7
Victor TOPORAN <victor.toporan@gmail.com>
2023-01-20 02:36:20 +0200

    Adds controller for user login

--- backend/springboot/src/main/java/tevi/msa/UserController.java
+++ backend/springboot/src/main/java/tevi/msa/UserController.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -16,12 +13,30 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> add(@RequestBody User user) {
+    public ResponseEntity<User> register(@RequestBody User user) {
         try {
             userService.saveUser(user);
             return new ResponseEntity<User>(user, HttpStatus.OK);
         } catch (DataIntegrityViolationException e) {
             return new ResponseEntity<User>(HttpStatus.IM_USED);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        try {
+            User existingUser = userService.getUser(user.getUsername());
+            if (existingUser == null) {
+                throw new DataIntegrityViolationException("No such user");
+
+            }
+            if (! existingUser.getPassword().equals((Object) user.getPassword())) {
+                throw new DataIntegrityViolationException("Password doesn't match");
+            }
+
+            return new ResponseEntity<User>(user, HttpStatus.OK); 
+        } catch (DataIntegrityViolationException e) {
+            return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
         }
     }
 }
--- backend/springboot/src/main/java/tevi/msa/UserRepository.java
+++ backend/springboot/src/main/java/tevi/msa/UserRepository.java
@@ -3,4 +3,5 @@ package tevi.msa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
+    User findByUsername(String username);
 }
